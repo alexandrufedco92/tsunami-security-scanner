ffdfb02bb76bd5d319df3eb2b194736c13de3703
alexandrufedco92 <68126139+alexandrufedco92@users.noreply.github.com>
2020-07-10 20:28:24 +0100

    Judashfujhca’s lkjd
    K>DSjb’[ fiuhs’ilDCUIVHD\IFLASND’CJUXH FO’ISDHC FILHJDKSCF I hf\co;klhj\giounspofhauisodfhj\isadfhviouix
    [cpoweh]oiuu
    [po

--- common/src/main/java/com/google/tsunami/common/cli/CliOption.java
+++ common/src/main/java/com/google/tsunami/common/cli/CliOption.java
@@ -31,3 +31,132 @@ public interface CliOption {
    */
   void validate();
 }
+\cjqw AIsfdg'iocun \JQWACIORUEHCD \QHJWEIORU  hcyopasrjhwo  u3qyd
+  r9poqhg f]0892nqu
+  dr3i  h23f]98r
+    0p2o3ruhnf  [9u23yn r
+    u90p  ufhoiua2r3YU
+    -09 FIUN23RDP9  29U4NR0E[F2 DUNR09O 2UYB
+    0P[O342NJ
+    DR  \OI2UR309-
+        PUNF4DIORHG]   T90RUG
+        FN  9PH453DRU
+        N[0 POJ2H3R[F0-
+                    9 I[U2N\PI34LFR89 CUN\POQLWKEHSUIDRH
+                      PO23UJDORI  24HE90- RU
+                      \POI3HFR9Y  U
+                      0\1P[OD4EN 091IO32HYR0]9  23U
+                        4[PRO 2F[39R
+                        P [\O2JHDR89UUN
+                        -04IUR
+                          20]39ORHGV90] 2EUNR\CPOI  WG [ERIUC UN\C2QL3KRG [P  WUNRC
+                          0[  O2PV9UI 4R
+                          0 PO2 R[89PIU 2U3R
+                          0OP\V23 [98weeybfu
+                          \p  o23hr iuy 23ghrpo uc2]39[rv2  \opr\3'hge[98c  uenr]pio  23geuiryb u29p3oiryb cfiu wcyb r0]9\  p2u r]0['ov y tc]09po 2ur\npocq]09rcui
+                            [op\23ryc [98 r 90po  2rtu9x  390 ricoph2 iu3[3cr \90op2  juh3reuwR90IO UC2\R3CY B]09 2IU 5R-POI32  QYR0]98CNU P3\RU2 I3R] \  2-CPOU3RNC[9V23NUPO890[ EF
+                            -90OP B3RIOEBCY   ]089IOR5UN2 IOP3RCYB 79[8W3 YRV]9PO 2VUHIUWavdsg[or8qivr
+                              po2v[897 uwcuy4
+                              n -v10\fr ]89cybr\op  2u3ncf89g f]09  oi45h 3riuefx]9po14ue[9c8 urvc
+                              90rutfg v]092piorj3hnd  -0]1uqyex9[8iu4po
+                              1c42u4y]089 2yhr]npo' 2ujnh[ox8r3ybd]09 i3un vct9 r2yf [iu  23nu]riov89[wcunrf\po2rt8o  3b v90po  2ucnr \p923 uqy]r-09cofeh[  whrfge  [90vrpcorg [98  werthiolgbh v n 184v=50ptnuc0-9ruy  ]2093iprudf0
+                                                        -=23  runc098i32r \v90  t4g 0894  inur\p0i9co2v3u tf9089n2mci 92v-]ruy3v[b f9vuio 34htoivfu
+                                                        n]  ociu24gh5]v p o2jue[08ry  2]iorh v  -93uyf b8voi 5\n  wcqef-]90io hn3]2rioqcwenu
+                                                          0]9eiutvg[n9opc5unr3]0  8wefu9r
+                                                            [03uetvy]g089ruj
+                                                            cnp o53cf]089iourn  ]09pur
+                                                            -90v2 34n
+                                                            -cr0ef9un 2
+  3-9ioprnv
+  gpircunv -90un
+    -r3fnc]0io5rtny]  09v2p3onru]0v9  [ucnr3
+    p0o\fnuvoq4wtievcn0
+      -[uvn2]09[wenuf
+      p0o ;2nrcpun
+        p]cor3ufn0]9c iovtugn
+        -0vpo32cu
+        r 90tvigtp9]oc4u3c[0v89 g9un8c 1fp2iou4hcn1 02upo[iugn
+        fcou4 1]t[rnu 40]8 hr3c0  [p95nt  c9048enfucikt2jh rn]940cygr[piumqw8
+        r9qugnvw]gcntg978r-d1ropu[ybcn  q3c0w8efe ty35t
+        nf-0c n24rq3ducp9iqtvwngr-0cf un90]8[4rh';vswqw3n [890poci  runt9[gqruwpoe2g[birunuqv
+                                             9-]0pcunp  c9oih[y]08idp 'chv[qwn8cr
+                           qep  nu]0w9UY0N9PI34UQGV78N78RC  ]3GU[8NV G=09IVOUQHP N  [9Cuyhoi2d4y[crg]0c pn[xiuec[r89wy]09opi  4uh[iquw4yeags9pu'oikgr[ya898e]-0cr9u3h[' gy8cB 8
+                                                                                                                                  W-R0IRUP9 ]OIRYFG[9RYCU
+                                                                                                                                  -0R O[PETV89GRYB] 0C9POURNOVI' UT W[uiokdvi
+                                                                                                                                                    qwopiuf9n cqr;it u9enchwgr908-
+                                                                                                                                                     gar]gunuc  rouvgirnqctr9 uig hnu8r-9iwoteuhny[u9pwoRCNU]90R8ucqgp\oie[gxoiufnu
+                                                                                                                                                     cp0qworunhv  wecuf
+                                                                                                                                                     0[po \ihv9r=cqwpi3rdou  ncq]vrpg 9ci9m-t=0
+                                                                                                                                                     ghcmr0iponr98cuR T90In]098ifunp  ocirut[098CYH3R'OIGRVB C  9 =-VI UR089TCBN  Q309]PORENUCLjwaeskugpf7c890
+                                                                                                                                                     9[0r3p gvr
+                                                                                                                                                     0  -ch[09QNUP9OIRYT[ N-8EIPOCIR[9 8ucs\p0r oirtc]-9QR-]8
+                                                                                                                                                     -\ceg  [8cr9=
+                                                                                                                                                     [-E4YR TVGYC = -IOUNC R0]  78n u0prtocUNCQ90JN*opruv y89etfcunn
+                                                                                                                                                     90po'rgh vAW890]89RC
+                                                                                                                                                     =0-POIV  NCTGUIY]U 0-4 1Un[fgjc8
+                                                                                                                                                     -=0iunr79cy 7
+                                                                                                                                                     -0[itqcnm9-0aurcn  \-0po 'mrQ3[PUYYNU
+                                                                                                                                                     1-[URy908rnu
+                                                                                                                                                     c-0  p[o3gr[87c9r
+                                                                                                                                                     m9=roug[4crn=0[co  pj]-s9euyhwrcnc]-mgyT7C9U OIMIRP9osnud
+                                                                                                                                                     [\porc 3es90unfc
+                                                                                                                                                     poikw  l4vehvijdo8ir2clp;k]ghm]-v0p[ruY]09VPAOSEip 
+                                                                                                                                                      ncrn8FU9]0IO['C 3RM[seuy]d8a09[oudsrwmei  ct=h8vgn79huim  c3
+                                                                                                                                                      rvt s09IJ CRNMtg] b79cfu[ql kd;4
+                                                                                                                                                      pco9vy8i3 qlcrvtg0 7y8  HC]IROP'; LMRt 980[uc24nmags ]87ugier hijm2 ercqgbgiu ohrwq3im  o2erc0y89im0e3  xerc8yr h[cp  om\iu [a97grhjn
+                                                                                                                                                      40[ouc]908wt8c  
+                                                                                                                                                      0[PO  RHW[7ae8
+                                                                                                                                                      =-0iuvtgn09 dueitoajntvg0
+                                                                                                                                                      nufyv978ugt powqgh[u isyufl;CVNGHUIUZSEoujhrt [iutyz]opju o[pr0c-09YR \ IORT  H3]89ayfu
+                                                                                                                                                      0p[ojrcn 
+                                                                                                                                                      0-IU
+                                                                                                                                                      P ORJKC[p0n]8qurt[voiugsyv89ier\c ovreg f7v8yur \po;cl besv97c8
+                                                                                                                                                      0[\po'  tmae9m8uuirumjucip[vtg obg89nnuirc
+                                                                                                                                                      [ p3;cv8osanufr
+                                                                                                                                                      po  cmirt
+                                                                                                                                                      g0f9yageiu  hm  \cru RM=Y890U0IRC
+                                                                                                                                                      [\PO;Q3RTWU
+                                                                                                                                                      a-09uihj   om 
+                                                                                                                                                      g 09uFUYPI R[OCR3
+                                                                                                                                                      3ewn s7a09ijrc  p[0qrwefmG NY8F97GUICJR M
+                                                                                                                                                        efa8y 9f7gyiuc  oormi
+                                                                                                                                                        tM98A-
+                                                                                                                                                        U9POCJR]e09 syangsui  ci 
+                                                                                                                                                        ef9gy cn89oi  rx
+                                                                                                                                                        c0ef9gny 8oir cm
+                                                                                                                                                         gfY89NOIC IR
+                                                                                                                                                         FEG AN7Y]9[OUICILJ R-f
+                                                                                                                                                         a gn8y8g9[ uacoii ra0e fg9fs8byn[9c  3r 
+                                                                                                                                                         gfyrb08coi jernfpsa9d8 ugpiucjrh [dufyvge[9dioefh gauddfya[8oiu  r hkhjsdc9ruic\pre un
+                                                                                                                                                         -90iocrq =-0opuY E[O C'JL REFsy gurih e
+                                                                                                                                                         s0dny t[f987rjcv nrt0fg]yS80IOUH CR  ef gstv7g8
+                                                                                                                                                         r[  cojhf890 dbsc  iocorm tgn80fqxuh im[rc gfy7t9 [cm  4 [
+                                                                                                                                                         rgp f0cn8-]xmioh2 xrgn a9]scqbpadf 9uoi  qx
+                                                                                                                                                         rcf9]ab8 yvcq] piorh eiu sdyn0oxpercF
+                                                                                                                                                         G9USY8GCUOL   MCI\R
+                                                                                                                                                         ep9seych]iop ;mcire 9g[odush cj  \p[cerevyg b89uovicj  23kmdcrvg UJMS[3
+                                                                                                                                                          2ERCQGISUFMC
+                                                                                                                                                          [R9]0grv8 hij den2rct8y9gpr hiefjom nrc8ygquafhc9p  owirhfs89 d08UIS
+                                                                                                                                                           \P RIT'VW]mryg[cow3JCRPIOFGUVN9XINuei
+                                                                                                                                                           [pol;rchg ]nr898 i
+                                                                                                                                                           [\cop; klrc ]n9eFN]KL  XJ4 cr[op0sgr9nafuqij 34
+                                                                                                                                                           [x;rtcgrafisy curei  lk's]cf0N GVN[H CIPOX2J EX][c9n8grys]jk rc3gciwfyb 9powiqhrcb [ ieufyu
+                                                                                                                                                           -09  poihne [c89r t]-9HN 3JKNGR PFURVGNY]9PO CRH89[Uyrt90po  4urc[khwy e[fc8oiqwu
+                                                                                                                                                           \r0poci  wg-
+                                                                                                                                                           C-\[ROI  CU[9WUF IPOKJR HGOUFBIA89[OI  RN\M Cgrfbun
+                                                                                                                                                            8ung r  pornf087gv[n  eircefanfg[ociwo4nuq30[W9IUJR
+                                                                                                                                                            C PODFNX79UMUD
+                                                                                                                                                            X30[OCP RIywrne79uoi  oq2liue]f8v79g fu[hodi3nc efgy7 ]vw80xrocii poqi'89AOIU
+                                                                                                                                                            POI CR3EUsdu0ieuco[igrda9uEE F7D88u0[ poiqrefnx[89dune
+                                                                                                                                                            po  ic3rgy
+                                                                                                                                                            u-0c9YHG]COE'CQ\MWID]spyage[irhcma0sd]zncg7f8 -9ur3e0
+                                                                                                                                                            -9f[n-[r 09qpWABNXFG7QNC]-9PO4H2[ Q4gy7fa-9ponc ]crf8 yctg[c97 yi nu
+                                                                                                                                                             e9-sgfuy 
+                                                                                                                                                             809uirc n]prgnudf]c089 ju  
+                                                                                                                                                             4c[q0fsdyf=s890icr 
+                                                                                                                                                              ]HYNA890[OUIC Ne89[puin 
+                                                                                                                                                              proq3edaioh rcipesgnya98sudsui
+                                                                                                                                                              po]iaelgviuxf qu8wi
+                                                                                                                                                              ocrN 89OIUR ]NCPIL'[agb r[98soiain8 
+                                                                                                                                                              0q3-[ojhcfluyf'oihiuyg]9
+                                                                                                                                                              iopawUYS908D  Q2PONY]q8[x[ue  p'JU9Q  UWN098E2}
